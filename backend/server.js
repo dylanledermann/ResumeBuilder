73973d8f9f306f26a1c91ee95240d75db1b0c827
dylhead <dylhead2003@gmail.com>
2025-05-05 18:00:03 -0700

    signup popup and login popup finished. Backend set up and MongoDB connection made

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+
+const app = express();
+
+//Middleware to handle CORS
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"], 
+    })
+);
+
+//Connect Database
+connectDB();
+
+//Middleware
+app.use(express.json());
+
+//Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- frontend/resume-builder/src/components/Modal.jsx
+++ frontend/resume-builder/src/components/Modal.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 const Modal = ({children, isOpen, onClose, title, hideHeader, showActionBtn, 
     actionBtnIcon=null, actionBtnText, onActionClick}) => {
+  if(!isOpen) return null;
+    
   return (
     <div className='fixed inset-0 z-50 flex justify-center items-center w-full h-full bg-black/40'>
       <div className={`relative flex flex-col bg-white shadow-lg rounded-lg overflow-hidden`}>
