35f4916135e9abdf97da4e7cb885218201322448
dylhead <dylhead2003@gmail.com>
2025-06-25 19:14:39 -0700

    ordering adjustment

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
+const router = express.Router();
+
 const { registerUser, loginUser, getUserProfile} = require("../controllers/authController");
 const { protect } = require("../middlewares/authMiddleware");
 const upload = require('../middlewares/uploadMiddleware');
-
-const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
